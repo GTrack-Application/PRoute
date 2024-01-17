b0808e969705d1f8590993949bb73f76822d5f57
Wasim <wasimxaman13@gmail.com>
2024-01-17 10:19:09 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nb_utils/nb_utils.dart';
-import 'package:p_route/Screens/assign/assign_screen1.dart';
-import 'package:p_route/Screens/route_management/route_management_screen.dart';
 import 'package:p_route/global/themes/themes.dart';
 import 'package:p_route/old/screens/login/user_login_page.dart';
 
